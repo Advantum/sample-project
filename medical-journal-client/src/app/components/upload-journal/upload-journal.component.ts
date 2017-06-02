b9996be7af9910bb64088320fce740fdfe86496c
proxis-ja <proxis.ja@gmail.com>
2017-06-02 12:03:40 -0500

    Updating validation for uplaoding

--- medical-journal-client/src/app/components/upload-journal/upload-journal.component.ts
+++ medical-journal-client/src/app/components/upload-journal/upload-journal.component.ts
@@ -13,34 +13,33 @@ import { UserService } from '../../services/user.service';
 export class UploadJournalComponent implements OnInit {
 
     public newJournalEntry = new Journal();
+    hasPermission: boolean;
   
   constructor(private journalService: JournalService, private userService: UserService) { }
 
   ngOnInit() {
-
+    const user = this.userService.getLocalValues();
+    if(user.userRole == 'Publisher'){
+      this.hasPermission = true;
+    }else{
+      this.hasPermission = false;
+    }
   }
 
   fileChange(event) {
-    
     let files = event.target.files;
     if (files.length > 0) {
       this.newJournalEntry.file = files[0];     
     }
     console.log(this.newJournalEntry);
+  }
+
+  upload(){
     this.journalService.saveJournal(this.newJournalEntry).subscribe(data =>{
       console.log(data);
     }, err => {
       console.log(err);
     });
-  }
-
-  hasPermission(){
-    const user = this.userService.getLocalValues();
-    if(user.userRole == 'Publisher'){
-      return true;
-    }else{
-      return false;
-    }
   }
 
 }
