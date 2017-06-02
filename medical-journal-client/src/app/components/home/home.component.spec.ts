fe75eaaf20e490cd27040a33760c69e7fda8a057
proxis-ja <proxis.ja@gmail.com>
2017-06-02 01:14:30 -0500

    adding home tests

--- medical-journal-client/src/app/components/home/home.component.spec.ts
+++ medical-journal-client/src/app/components/home/home.component.spec.ts
@@ -22,4 +22,18 @@ describe('HomeComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Medical Journal');
+  }));
+
+  it('should render welcome in a p tag', async(() => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain('Welcome');
+  }));
 });
