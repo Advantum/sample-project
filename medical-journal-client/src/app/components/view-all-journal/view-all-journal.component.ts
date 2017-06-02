36cc8181841a47539e0bdf1425bca9ec9908146a
mashbourne3 <marlandoashbourne@yahoo.com>
2017-06-01 20:03:05 -0500

    Modify view all Journals. Pull mock journals from client side

--- medical-journal-client/src/app/components/view-all-journal/view-all-journal.component.ts
+++ medical-journal-client/src/app/components/view-all-journal/view-all-journal.component.ts
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllJournalComponent implements OnInit {
 
-  constructor() { }
+  journals = [
+  {
+    "Name": "Jane Doe",
+    "Description": "Diabetes is dangerous",
+    "FileUrl": "/fileupload/Diabetes.pdf",
+    "Publisher": "Jane Doe",
+    "DatePublished": "May 10,2017"
+  },
+  {
+    "Name": "Jane Doe",
+    "Description": "Cancer is critical",
+    "FileUrl": "/fileupload/Diabetes.pdf",
+    "Publisher": "Jane Doe",
+    "DatePublished": "February 1,2017"
+  },
+  {
+    "Name": "Rick Doe",
+    "Description": "Diabetes is dangerous",
+    "FileUrl": "/fileupload/Diabetes.pdf",
+    "Publisher": "Jane Doe",
+    "DatePublished": "January 1,2017"
+  },
+  {
+    "Name": "Mary Doe",
+    "Description": "Jane Doe",
+    "FileUrl": "/fileupload/Diabetes.pdf",
+    "Publisher": "Jane Doe",
+    "DatePublished": "May 1,2016"
+  }
+ ];
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
